b65186150b5f402696bcd9d6f2a0395395ac9581
aldin <a21aldsa@student.his.se>
2022-05-03 14:39:39 +0200

    created shared preference

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    TextView TextViewName;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextViewName = findViewById(R.id.name);
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("name", "Tomten");
+        editor.apply();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String name = preferences.getString("name", "inget namn hittades");
+        TextViewName.setText(name);
+    }
 }
